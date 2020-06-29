884129b7c9962854d63b5887fb1c60393ffc3a10
Ian Peake <ian.peake@rmit.edu.au>
2020-06-29 12:17:47 +1000

    foreign relay (used for mir100)

---
+++ rosie/foreign_relay.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+import xmlrpclib
+import urlparse
+
+USAGE = 'foreign_relay.py {adv|sub} topic foreign_master_uri'
+
+g_publishers = {}
+
+# Clean up the stuff that we registered
+def shutdown(foreign_master_uri):
+    server = xmlrpclib.ServerProxy(foreign_master_uri)
+    for p in g_publishers:
+        server.unregisterPublisher(p, topic, g_publishers[p])
+        print 'unregistering %s @ %s'%(p,g_publishers[p])
+
+def go(mode, topic, foreign_master_uri, update_period):
+    print 'go'
+    publishers = {}
+    if mode == 'sub':
+        sub_uri = foreign_master_uri
+        adv_uri = os.environ['ROS_MASTER_URI']
+    else:
+        sub_uri = os.environ['ROS_MASTER_URI']
+        adv_uri = foreign_master_uri
+    print 'subscribe uri',sub_uri
+    print 'advise uri',adv_uri
+    server_sub = xmlrpclib.ServerProxy(sub_uri)
+    server_adv = xmlrpclib.ServerProxy(adv_uri)
+    while 1:
+        code, msg, topics = server_sub.getPublishedTopics('', '')
+        # Determine the type of the topic
+        type = None
+        for t in topics:
+            #print 'topic',t
+            if t[0] == topic:
+                #print 'topic match'
+                type = t[1]
+                break
+        # Find publishers
+        if type is not None:
+            #print 'pub',t
+            new_publishers = {}
+            code, msg, state = server_sub.getSystemState('')
+            pubs = state[0]
+            for t in pubs:
+                if t[0] == topic:
+                    #print 'topic match'
+                    for p in t[1]:
+                        # Hack to avoid circular re-subscription
+                        dup = False
+                        for pp in publishers:
+                            if p.startswith(pp):
+                                #print '%s is dup of %s; skipping'%(p,pp)
+                                dup = True
+                                break
+                        if dup:
+                            continue
+                        code, msg, uri = server_sub.lookupNode('', p)
+                        # Uniquify by master
+                        sp = urlparse.urlsplit(sub_uri)
+                        mangled_name = '%s_%s_%s'%(p,sp.hostname,sp.port)
+                        new_publishers[mangled_name] = uri
+            # Update remote advertisements appropriately
+            subtractions = set(publishers.keys()) - set(new_publishers.keys())
+            additions = set(new_publishers.keys()) - set(publishers.keys())
+            for s in subtractions:
+                server_adv.unregisterPublisher(s, topic, publishers[s])
+                print 'on ',server_adv
+                print 'unregistering %s @ %s'%(s,publishers[s])
+            for a in additions:
+                server_adv.registerPublisher(a, topic, type, new_publishers[a])
+                print 'on ',server_adv
+                print 'registering %s @ %s'%(a,new_publishers[a])
+            publishers = new_publishers
+            global g_publishers
+            g_publishers = publishers
+        time.sleep(update_period)
+
+def parse(argvin):
+    argv = []
+    # Remove the ROS arguments.
+    for arg in argvin:
+        if arg.find(":=") == -1:
+            argv.append(arg)
+    if len(argv) != 4:
+        print USAGE
+        return None
+    mode = argv[1]
+    if mode != 'sub' and mode != 'adv':
+        print USAGE
+        return None
+    topic = argv[2]
+    foreign_master_uri = argv[3]
+    return (mode, topic, foreign_master_uri)
+
+if __name__ == '__main__':
+    ret = parse(sys.argv)
+    if ret is None:
+        sys.exit(1)
+    mode, topic, foreign_master_uri = ret
+    update_period = 1.0
+    try:
+        go(mode, topic, foreign_master_uri, update_period)
+    except KeyboardInterrupt, e:
+        if mode == 'adv':
+            shutdown(foreign_master_uri)
+        else:
+            shutdown(os.environ['ROS_MASTER_URI'])
